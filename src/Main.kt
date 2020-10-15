cbc87e4427425b4a1b9d1a951ce1f719b93bfc47
sina73azar <s_azarfarshi@outlook.com>
2020-10-15 04:52:40 +0330

    22 mehr added

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,16 @@
 fun main() {
-    println("Hello Kotlin")
-/*    var name="ali"
+    var a = 0
+    while (a < 5) {
+
+        //starPrint()
+        ++a
+    }
+    //fibonatci()
+
+}
+
+fun starter() {
+    /*    var name="ali"
     name="reza"
     print("test $name\n")
     val name1="sina"
@@ -40,28 +50,170 @@ fun main() {
 //
 //    println("Hello $name your age is ${1399-dateOfBirth!!.toInt()}")
 
-    //get3DigitNum()
 //    val floatArray:Array<Double> = arrayOf(1.1, 2.5,1.25798)
 //    println(floatArray.size)
 //    println(floatArray[2])
-
-    val array1=Array(6){i->i*3}
-    val array2=Array(10){i->"ali$i"}
-    println(array2[5])
-    println(array1[4])
-    val array3=Array(8){"sina$it"}
-    conditionStatement()
 }
+
 fun get3DigitNum() {
     println("Enter 3 digit number: ")
     var inputNum = readLine()
-    println(inputNum!!.toInt()>200 && inputNum!!.toInt()<500)
+    println(inputNum!!.toInt() > 200 && inputNum!!.toInt() < 500)
 }
+
 fun conditionStatement() {
+    println("Enter your Score")
     var userPoint = readLine()!!.toInt()
-    if (userPoint >= 70) {
-        println("pass")
-    } else {
-        println("fail")
+//    if (userPoint > 100) {
+//        println("enter under 100")
+//    } else if (userPoint > 80) {
+//        println("top player")
+//    } else if (userPoint > 50)
+//        println("passed")
+//    else if (userPoint > 30)
+//        println("condition pass")
+//    else
+//        println("fail")
+    var status: String
+    status = if (userPoint in 80..100) "top"
+    else if (userPoint in 50..80) "passed"
+    else if (userPoint in 30 until 50) "condition passed"
+    else "fail"
+    println(status)
+}
+
+fun arrayBuild() {
+    val array1 = Array(6) { i -> i * 3 }
+
+    val array2 = Array(10) { i -> "ali$i" }
+    println(array2[5])
+    println(array1[4])
+    val array3 = Array(8) { "sina$it" }
+}
+
+fun kotlinIfAdvantage() {
+    var name: String
+    val num = readLine()!!.toInt()
+    name = if (num == 1) "sina" else if (num == 0) "javad" else "ghader"
+    println(name)
+
+}
+
+fun testOfWhen() {
+    println("Enter number")
+    var num = readLine()!!.toInt()
+    var outputNumber: String
+    outputNumber = when (num) {
+        1 -> {
+            println("testi")
+            "one"
+        }
+        2 -> "two"
+        3 -> "three"
+        4 -> "four"
+        5 -> "five"
+        6, 7, 8 -> "six"
+        else -> "yechi dige"
+    }
+    println(outputNumber)
+}
+
+fun testOfWhen2() {
+    println("Enter number")
+    var num = readLine()!!.toInt()
+    when (num) {
+        in 1..5 -> println("one to five")
+        in 5..10 -> println("five to ten")
     }
 }
+
+fun forTest() {
+    /*   for (i in 1..1000) {
+           if (i % 2 == 0) {
+               println(i)
+           }
+       }*/
+/*    for (i in 1..500)
+        println(i*2)*/
+//    for (i in 2..1000 step 2)
+//        println(i)
+//    for (i in 1400 downTo 110 step 5) {
+//        println("num $i")
+//    }
+/*    for (name in 0..2) {
+        println(arrayOfNames[name])
+    }*/
+//    for (name in arrayOfNames) {
+//        println(name)
+//    }
+    var arrayOfNames = arrayOf("sina", "ali", "reza", "mahtab")
+    for (index in arrayOfNames.indices) {
+        println("$index is ${arrayOfNames[index]}")
+    }
+//    var i=1
+//    while (i < 500) {
+//        println(i * 2)
+//        ++i
+//    }
+/*    var i=2000
+    do {
+        i++
+        println(i)
+    }while (i<1000)*/
+//    var i=0
+//    do {
+//        ++i
+//        println(i*2)
+//    }while (i<500)
+
+    //task : fibonatcci sequence 20 number
+
+}
+
+fun conAndBreakSample() {
+    var i=0
+    while (i < 100) {
+        i++
+        if (i % 5 == 0) continue
+        println(i)
+    }
+    repeat(30) {
+        println(it)
+    }
+
+}
+
+fun starPrint() {
+    println("Enter number of Star rows:")
+    val rows= readLine()!!.toInt()
+    for (i in 1..rows) {
+        for (j in 1..i) {
+            print("*")
+        }
+        print("\n")
+    }
+}
+//Question:how can we fill an empty array?
+fun fibonatci() {
+    println("Enter count of numbers of fibonatci you need?")
+    val arraySize = readLine()!!.toInt()
+    var fn=0
+    var fnPlus=1
+    var fnPlusPlus:Int
+    print("$fn\t\t")
+    print("$fnPlus\t\t")
+    for (i in 1..arraySize-2) {
+        fnPlusPlus=fn+fnPlus
+        print("$fnPlusPlus\t\t")
+        if(i==8) println("")
+        if (i > 18) {
+
+            if(i%10==0) println("")
+        }
+        fn=fnPlus
+        fnPlus=fnPlusPlus
+    }
+
+
+}
+
